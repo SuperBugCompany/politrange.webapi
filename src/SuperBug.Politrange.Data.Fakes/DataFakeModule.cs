988b5a09c028efe4a6c93582aad29facafc202a9
iGilga <pyua87@gmail.com>
2015-11-30 01:35:10 +0300

    Регистрация модуля.

---
+++ src/SuperBug.Politrange.Data.Fakes/DataFakeModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using SuperBug.Politrange.Data.Fakes.Repositories;
+using SuperBug.Politrange.Data.Repositories;
+
+namespace SuperBug.Politrange.Data.Fakes
+{
+    public class DataFakeModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<SiteRepositoryFake>().As<ISiteRepository>().SingleInstance();
+        }
+    }
+}
