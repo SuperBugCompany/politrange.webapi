aa2617ac0a79d29e5da029d0c36bba5d1a82bdad
iGilga <pyua87@gmail.com>
2015-12-01 00:18:15 +0300

    Добавлен интерфейс репозитория страниц

---
+++ src/SuperBug.Politrange.Data/Repositories/IPageRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using SuperBug.Politrange.Models;
+
+namespace SuperBug.Politrange.Data.Repositories
+{
+    public interface IPageRepository
+    {
+        IEnumerable<Page> GetPages();
+        Page GetPageById(int id);
+        Page AddPage(Page page);
+        bool DeletePage(int id); 
+    }
+}
