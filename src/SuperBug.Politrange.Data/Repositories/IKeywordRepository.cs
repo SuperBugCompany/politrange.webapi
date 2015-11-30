70e16716e2eb79601cd62f1229cdae80d8293057
iGilga <pyua87@gmail.com>
2015-11-30 23:59:15 +0300

    Добавлен интерфейс репозитория ключевых слов

---
+++ src/SuperBug.Politrange.Data/Repositories/IKeywordRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using SuperBug.Politrange.Models;
+
+namespace SuperBug.Politrange.Data.Repositories
+{
+    public interface IKeywordRepository
+    {
+        IEnumerable<Keyword> GetKeywords();
+        Keyword GetKeywordById(int id);
+        Keyword AddKeyword(Keyword keyword);
+        bool DeleteKeyword(int id); 
+    }
+}
