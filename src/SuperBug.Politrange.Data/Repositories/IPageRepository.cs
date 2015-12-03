0e4d7644fbffc4412849a50e8ceb6d9582ccebcc
iGilga <pyua87@gmail.com>
2015-12-03 15:22:01 +0300

    Добавлена реализация фильтра страниц

--- src/SuperBug.Politrange.Data/Repositories/IPageRepository.cs
+++ src/SuperBug.Politrange.Data/Repositories/IPageRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using SuperBug.Politrange.Models;
 
 namespace SuperBug.Politrange.Data.Repositories
@@ -6,6 +7,7 @@ namespace SuperBug.Politrange.Data.Repositories
     public interface IPageRepository
     {
         IEnumerable<Page> GetPages();
+        IEnumerable<Page> GetMany(Func<Page, bool> where);
         Page GetPageById(int id);
         Page AddPage(Page page);
         bool DeletePage(int id); 
