f31688f9f7c10e5dc446843eefb865771cc61851
iGilga <pyua87@gmail.com>
2015-12-06 22:14:30 +0300

    Добавлен консоль для развертывания локального веб-сервиса.

---
+++ src/SuperBug.Politrange.WebAp.SelfHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using SuperBug.Politrange.Api;
+
+namespace SuperBug.Politrange.WebAp.SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:10101"))
+            {
+                Console.WriteLine("Web Server is running.");
+                Console.WriteLine("Press any key to quit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
