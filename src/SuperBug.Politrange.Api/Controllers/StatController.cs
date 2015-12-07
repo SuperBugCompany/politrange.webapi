2975709164aac048c227c90b217209aeb8338f2e
iGilga <pyua87@gmail.com>
2015-12-07 20:18:54 +0300

    Добавлен мтеод статистики за период даты

--- src/SuperBug.Politrange.Api/Controllers/StatController.cs
+++ src/SuperBug.Politrange.Api/Controllers/StatController.cs
@@ -17,15 +17,23 @@ namespace SuperBug.Politrange.Api.Controllers
             this.statService = statService;
         }
 
-        public IHttpActionResult GetStatBySiteId(int id)
+        public IHttpActionResult GetBySiteId(int id)
         {
-            IEnumerable<PersonPageRank> ratings = statService.GetPageRanksBySite(id);
+            IEnumerable<PersonPageRank> ratings = statService.GetRanksBySite(id);
 
             var ratingsViewModel = Mapper.Map<IEnumerable<PersonPageRank>, IEnumerable<CommonStatViewModel>>(ratings);
 
             return Ok(ratingsViewModel);
         }
-    }
 
-    
+        public IHttpActionResult GetByRangeDate(int id, DateTime begin, DateTime end)
+        {
+            var ratings = statService.GetRanksByRangeDate(id, begin, end);
+
+            IEnumerable<RangeStatViewModel> ratingsViewModel =
+                Mapper.Map<IEnumerable<PersonPageRank>, IEnumerable<RangeStatViewModel>>(ratings);
+
+            return Ok(ratingsViewModel);
+        }
+    }
 }
