3bb0920659dd925f9a8cb9ee4e49d5d1a0ccaa8c
iGilga <pyua87@gmail.com>
2015-12-08 00:58:49 +0300

    Переделан контекст поддержки соединения с mysql

--- src/SuperBug.Politrange.Data/Contexts/PolitRangeContext.cs
+++ src/SuperBug.Politrange.Data/Contexts/PolitRangeContext.cs
@@ -1,13 +1,25 @@
 ﻿using System.Data.Entity;
+using MySql.Data.Entity;
 using SuperBug.Politrange.Models;
 
 namespace SuperBug.Politrange.Data.Contexts
 {
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
     public class PolitrangeContext: DbContext, IPolitrangeContext
     {
         public PolitrangeContext()
             : base("PolitrangeContext")
         {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Keyword>().MapToStoredProcedures();
+            modelBuilder.Entity<Person>().MapToStoredProcedures();
+            modelBuilder.Entity<PersonPageRank>().MapToStoredProcedures();
+            modelBuilder.Entity<Page>().MapToStoredProcedures();
+            modelBuilder.Entity<Site>().MapToStoredProcedures();
         }
 
         public DbSet<Keyword> Keywords { get; set; }
