31b7a8b274fe6e3d00437e0cb7090f92b91b6d23
iGilga <pyua87@gmail.com>
2015-12-07 20:18:10 +0300

    Добавлен дто для вывода периода статистики

---
+++ src/SuperBug.Politrange.Api.Models/ViewModels/RangeStatViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SuperBug.Politrange.Api.Models.ViewModels
+{
+    public class RangeStatViewModel
+    {
+        public string PersonName { get; set; }
+        public int Rank { get; set; }
+        public DateTime PageFoundDate { get; set; }
+    }
+}
