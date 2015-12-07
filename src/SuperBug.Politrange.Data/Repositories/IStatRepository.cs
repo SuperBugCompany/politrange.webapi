27aaccf9a6482de24b174de4cb0a04b1771518e7
iGilga <pyua87@gmail.com>
2015-12-07 20:14:43 +0300

    Добавлена сигнатура в интерфейсе

--- src/SuperBug.Politrange.Data/Repositories/IStatRepository.cs
+++ src/SuperBug.Politrange.Data/Repositories/IStatRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using SuperBug.Politrange.Models;
 
@@ -6,5 +7,6 @@ namespace SuperBug.Politrange.Data.Repositories
     public interface IStatRepository
     {
         IEnumerable<PersonPageRank> GetPageRanksBySite(int siteId);
+        IEnumerable<PersonPageRank> GetPageRanksByRangeDate(int siteId, DateTime beginDate, DateTime endDate);
     }
 }
