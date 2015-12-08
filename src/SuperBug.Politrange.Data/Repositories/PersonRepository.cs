ea8662fa7336ae5f36701d0bf1db79cc6463bdc1
iGilga <pyua87@gmail.com>
2015-12-09 00:01:06 +0300

    Реализованы методы

--- src/SuperBug.Politrange.Data/Repositories/PersonRepository.cs
+++ src/SuperBug.Politrange.Data/Repositories/PersonRepository.cs
@@ -1,33 +1,63 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using SuperBug.Politrange.Data.Contexts;
 using SuperBug.Politrange.Models;
 
 namespace SuperBug.Politrange.Data.Repositories
 {
     class PersonRepository: IPersonRepository
     {
+        private readonly IPolitrangeContext context;
+
+        public PersonRepository(IPolitrangeContext context)
+        {
+            this.context = context;
+        }
+
         public IEnumerable<Person> GetAll()
         {
-            throw new System.NotImplementedException();
+            return context.Persons.ToList();
         }
 
         public Person GetById(int id)
         {
-            throw new System.NotImplementedException();
+            return context.Persons.Find(id);
         }
 
         public Person Add(Person entity)
         {
-            throw new System.NotImplementedException();
+            entity = context.Persons.Add(entity);
+            context.SaveChanges();
+            return entity;
         }
 
         public bool Update(Person entity)
         {
-            throw new System.NotImplementedException();
+            bool isUpdated = false;
+            context.Persons.AddOrUpdate(entity);
+            if (context.SaveChanges() > 0)
+            {
+                isUpdated = true;
+            }
+            
+            return isUpdated;
         }
 
         public bool Delete(int id)
         {
-            throw new System.NotImplementedException();
+            bool isDeleted = false;
+
+            var person = GetById(id);
+
+            if (person != null)
+            {
+                context.Persons.Remove(person);
+                context.SaveChanges();
+                isDeleted = true;
+            }
+
+            return isDeleted;
         }
     }
 }
