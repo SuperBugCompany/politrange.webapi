bf59cb9046ec5fec2b6daa1703fe7ddb93f861ed
iGilga <pyua87@gmail.com>
2015-12-10 18:52:52 +0300

    Добавлен ключ страниц

--- src/SuperBug.Politrange.Models/PersonPageRank.cs
+++ src/SuperBug.Politrange.Models/PersonPageRank.cs
@@ -7,6 +7,7 @@ namespace SuperBug.Politrange.Models
 	    public int PersonPageRankId { get; set; }
 	    public int PersonId { get; set; }
 		public Person Person { get; set; }
+	    public int PageId { get; set; }
 		public Page Page { get; set; }
 		public int Rank { get; set; }
 	}
