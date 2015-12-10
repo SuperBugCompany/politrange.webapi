d1b026fbfd7d515c49c9ab1d37181f08b5b6a28b
iGilga <pyua87@gmail.com>
2015-12-10 18:49:09 +0300

    Вынесен метод, возвращающий статус запроса от условия.

--- src/SuperBug.Politrange.Api/Controllers/KeywordController.cs
+++ src/SuperBug.Politrange.Api/Controllers/KeywordController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Http;
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
 using AutoMapper;
 using SuperBug.Politrange.Api.Models.ViewModels;
 using SuperBug.Politrange.Models;
@@ -18,24 +21,28 @@ namespace SuperBug.Politrange.Api.Controllers
         public IHttpActionResult Put(int id, KeywordViewModel keywordViewModel)
         {
             var keyword = Mapper.Map<KeywordViewModel, Keyword>(keywordViewModel);
+
             keyword.KeywordId = id;
 
             bool isUpdated = keywordService.Update(keyword);
-            return Ok(isUpdated);
+
+            return GetResponseMessageResult(isUpdated);
         }
 
         public IHttpActionResult Delete(int id)
         {
-            bool isUpdated = keywordService.Remove(id);
+            bool isDeleted = keywordService.Remove(id);
 
-            if (isUpdated)
-            {
-                return Ok();
-            }
-            else
-            {
-                return NotFound();
-            }
+            return GetResponseMessageResult(isDeleted);
+        }
+
+        private IHttpActionResult GetResponseMessageResult(bool condition)
+        {
+            var httpResponseMessage = condition
+                ? new HttpResponseMessage(HttpStatusCode.OK)
+                : new HttpResponseMessage(HttpStatusCode.NotFound);
+
+            return new ResponseMessageResult(httpResponseMessage);
         }
     }
 }
