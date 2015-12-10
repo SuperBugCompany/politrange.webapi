95bec9e5cfa8e88ce797587e9087e89240710e6a
iGilga <pyua87@gmail.com>
2015-12-10 18:52:28 +0300

    Добавлен ключ персонажи

--- src/SuperBug.Politrange.Models/PersonPageRank.cs
+++ src/SuperBug.Politrange.Models/PersonPageRank.cs
@@ -5,6 +5,7 @@ namespace SuperBug.Politrange.Models
 	public class PersonPageRank
 	{
 	    public int PersonPageRankId { get; set; }
+	    public int PersonId { get; set; }
 		public Person Person { get; set; }
 		public Page Page { get; set; }
 		public int Rank { get; set; }
