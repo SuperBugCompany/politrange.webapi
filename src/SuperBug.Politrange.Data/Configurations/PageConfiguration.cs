41b00f24987ddff6c26fff67654095379d140d88
iGilga <pyua87@gmail.com>
2015-12-11 04:43:58 +0300

    Вынесен конфиг таблицы персонажи

---
+++ src/SuperBug.Politrange.Data/Configurations/PageConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using SuperBug.Politrange.Models;
+
+namespace SuperBug.Politrange.Data.Configurations
+{
+    public class PageConfiguration: EntityTypeConfiguration<Page>
+    {
+        public PageConfiguration()
+        {
+            HasRequired(s => s.Site)
+                .WithMany(s => s.Pages)
+                .HasForeignKey(s => s.SiteId);
+
+            Property(p => p.FoundDate)
+                .IsOptional();
+            Property(p => p.LastScanDate)
+                .IsOptional();
+        }
+    }
+}
