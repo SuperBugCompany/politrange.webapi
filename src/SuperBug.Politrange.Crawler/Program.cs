40dd0f4944e5c9dd78f2461515df7b66651d0fd1
kartavec <jenia@webkontrol.ru>
2015-12-12 11:22:08 +0300

    создан проект

---
+++ src/SuperBug.Politrange.Crawler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperBug.Politrange.Crawler
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+		}
+	}
+}
