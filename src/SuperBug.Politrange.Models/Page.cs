e40795639f36a28cd48734be6dafe1bb1fb0e2a4
iGilga <pyua87@gmail.com>
2015-12-17 20:06:54 +0300

    Изменен тип в модели

--- src/SuperBug.Politrange.Models/Page.cs
+++ src/SuperBug.Politrange.Models/Page.cs
@@ -7,8 +7,8 @@ namespace SuperBug.Politrange.Models
 	{
 		public int PageId { get; set; }
 		public string Uri { get; set; }
-		public DateTime FoundDate { get; set; }
-		public DateTime LastScanDate { get; set; }
+		public DateTime? FoundDate { get; set; }
+		public DateTime? LastScanDate { get; set; }
 	    public int SiteId { get; set; }
 		public virtual Site Site { get; set; }
 	    public virtual ICollection<PersonPageRank> PersonPageRanks { get; set; }
