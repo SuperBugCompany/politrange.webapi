2384d586a6ddcf079cae015078b59a11effe113e
iGilga <pyua87@gmail.com>
2015-12-18 17:44:04 +0300

    Вынесен в файл. Убран наследование интерфейса.

---
+++ src/SuperBug.Politrange.Data/Repositories/IPersonPageRankRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using SuperBug.Politrange.Data.Infrastructure;
+using SuperBug.Politrange.Models;
+
+namespace SuperBug.Politrange.Data.Repositories
+{
+    public interface IPersonPageRankRepository
+    {
+        void Insert(IEnumerable<PersonPageRank> entities);
+    }
+}
