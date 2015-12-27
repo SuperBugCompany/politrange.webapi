eda4b7307c3d77c58f26df21a6070af76d4af024
iGilga <pyua87@gmail.com>
2015-12-27 16:46:08 +0300

    Добавлен регистрация NLogModule

--- src/SuperBug.Politrange.Data/DataModule.cs
+++ src/SuperBug.Politrange.Data/DataModule.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using Autofac;
+using Autofac.Extras.NLog;
 using SuperBug.Politrange.Data.Contexts;
 using Module = Autofac.Module;
 
@@ -9,6 +10,8 @@ namespace SuperBug.Politrange.Data
     {
         protected override void Load(ContainerBuilder builder)
         {
+            builder.RegisterModule<NLogModule>();
+
             builder.RegisterAssemblyTypes(Assembly.Load("SuperBug.Politrange.Data"))
                    .Where(t => t.Name.EndsWith("Repository"))
                    .AsImplementedInterfaces()
