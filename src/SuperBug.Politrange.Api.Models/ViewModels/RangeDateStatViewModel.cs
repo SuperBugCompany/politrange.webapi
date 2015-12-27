7cbe714be5c68cf4b676cb2076452c7382f7aaff
iGilga <pyua87@gmail.com>
2015-12-27 07:10:46 +0300

    Добавлено DTO

---
+++ src/SuperBug.Politrange.Api.Models/ViewModels/RangeDateStatViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using SuperBug.Politrange.Models;
+
+namespace SuperBug.Politrange.Api.Models.ViewModels
+{
+    public class RangeDateStatViewModel
+    {
+        public DateTime Date { get; set; }
+        public IEnumerable<PersonRankViewModel> Persons { get; set; } 
+    }
+
+    public class PersonRankViewModel
+    {
+        public string Name { get; set; }
+        public int Rank { get; set; }
+    }
+}
