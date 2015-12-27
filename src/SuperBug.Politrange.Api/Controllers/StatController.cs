1061d2bd8f36851f47fbc3e14ea84b3ebd5462ca
iGilga <pyua87@gmail.com>
2015-12-27 07:12:52 +0300

    Изменены типы

--- src/SuperBug.Politrange.Api/Controllers/StatController.cs
+++ src/SuperBug.Politrange.Api/Controllers/StatController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Web.Http;
 using AutoMapper;
+using SuperBug.Politrange.Api.Models.Models;
 using SuperBug.Politrange.Api.Models.ViewModels;
 using SuperBug.Politrange.Models;
 using SuperBug.Politrange.Services.States;
@@ -30,8 +31,8 @@ namespace SuperBug.Politrange.Api.Controllers
         {
             var ratings = statService.GetRanksByRangeDate(id, begin, end);
 
-            IEnumerable<RangeStatViewModel> ratingsViewModel =
-                Mapper.Map<IEnumerable<PersonPageRank>, IEnumerable<RangeStatViewModel>>(ratings);
+            IEnumerable<RangeDateStatViewModel> ratingsViewModel =
+                Mapper.Map<IEnumerable<RangeDatePersonRank>, IEnumerable<RangeDateStatViewModel>>(ratings);
 
             return Ok(ratingsViewModel);
         }
